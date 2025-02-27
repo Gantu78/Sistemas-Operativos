e035de1bfe420286a7fed7cf425534b86a883bae
Samuel Jerónimo Gantiva Garzón <71401479+Gantu78@users.noreply.github.com>
2025-02-27 10:09:01 -0500

    Update ejercicio.c

--- EJERCICIOS_EN_CLASE/ejercicio.c
+++ EJERCICIOS_EN_CLASE/ejercicio.c
@@ -1,1 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  int variable = 123;
+  int *ptr = &variable;
+  printf("Valor de variable: %d\n", variable);
+  printf("Dirección de variable: %p\n", &variable);
+  printf("Valor de ptr: %p\n", (void *)ptr);
+  printf("Valor al que apunta ptr: %i\n", *ptr);
+
+  return 0;
+}
