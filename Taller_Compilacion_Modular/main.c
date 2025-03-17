8e4e677c10fdd81356ec1c753eeff0cc7f97c6c1
Samuel Jerónimo Gantiva Garzón <71401479+Gantu78@users.noreply.github.com>
2025-03-17 02:03:33 +0000

    comentarios agregados

--- Taller_Compilacion_Modular/main.c
+++ Taller_Compilacion_Modular/main.c
@@ -12,33 +12,33 @@
 #include <string.h>
 #include "vector_dinamico.h"
 #include "matriz.h"
-
+/*Funcion para mostrar el menu para jugar con un vector de strings*/
 void vectorMenu(vectorDinamico *V) {
     int opcion, indice;
     char buffer[100];
     while (1) {
         printf("\n--- Menú Vectores ---\n");
-        printf("1. Añadir elemento\n2. Borrar elemento\n3. Modificar elemento\n4. Mostrar vector\n5. Volver\nOpción: ");
+        printf("1. Añadir elemento\n2. Borrar elemento\n3. Modificar elemento\n4. Mostrar vector\n5. Volver\nOpción: "); //Mostrar las opciones en pantalla
         scanf("%d", &opcion);
         switch (opcion) {
-            case 1:
+            case 1:                                                     //Caso para añadir elemento (opcion 1)
                 printf("Ingrese texto: ");
                 scanf("%s", buffer);
                 addVector(V, strdup(buffer));
                 break;
-            case 2:
+            case 2:                                                     //Caso para borrar elemento (opcion 2)
                 printf("Índice a borrar: ");
                 scanf("%d", &indice);
                 borrarVector(V, indice);
                 break;
-            case 3:
+            case 3:             //Caso para modificar elemento (opcion 2), necesita el indice que modificará y el texto por el que cambiará el elemento
                 printf("Indice a modificar: ");
                 scanf("%d", &indice);
                 printf("Indique el texto: ");
                 scanf("%s", buffer);
                 setVector(V, indice, strdup(buffer));
                 break;
-            case 4:
+            case 4:             // Caso para imprimir el vector
                 imprimirVector(V);
                 break;
             case 5:
@@ -47,21 +47,23 @@ void vectorMenu(vectorDinamico *V) {
     }
 }
 
+/*Funcion para mostrar el menu de las matrices*/
 void matrizMenu() {
     int N;
     int opcion;
     printf("Tamaño de la matriz (menor a 100): ");
     scanf("%d", &N);
+    /*Inicializar matrices A,B,C, C en 0*/
     int *mA = malloc(sizeof(int) * N * N);
     int *mB = malloc(sizeof(int) * N * N);
     int *mC = calloc(N * N, sizeof(int));
     iniMatriz(mA, mB, N);
     while (2) {
         printf("\n--- Menú Matrices ---\n");
-        printf("1. Imprimir matrices\n2. Multiplicar matrices para ver resultado en matriz C\n3. Volver\nOpción: ");
+        printf("1. Imprimir matrices\n2. Multiplicar matrices para ver resultado en matriz C\n3. Volver\nOpción: "); //Imprimir opciones de menu para matrices
         scanf("%d", &opcion);
         switch (opcion) {
-            case 1:
+            case 1:                     //Imprimirá todas las matrices, en caso de no haber multiplicado previamente la matriz C será 0 en todas sus posiciones
                 printf("Matriz A:\n");
                 printMatriz(mA, N);
                 printf("Matriz B:\n");
@@ -69,7 +71,7 @@ void matrizMenu() {
                 printf("Matriz C (A x B):\n");
                 printMatriz(mC, N);
                 break;
-            case 2:
+            case 2:           //Caso para multiplicar las matrices y guardar el resultado en la matriz C, una vez multiplicado volver a imprimir matrices para visualizar el resultado
                 multMatriz(mA, mB, mC, N);
                 printf("Matrices multiplicadas correctamente (vuelve a imprimir las matrices para ver el resultado en matriz C :) )\n");
                 break;
@@ -77,8 +79,8 @@ void matrizMenu() {
                 return;
         }
     }
-    
-    free(mA);
+    /*Liberar memoria de todas las matrices*/
+    free(mA); 
     free(mB);
     free(mC);
 }
@@ -89,16 +91,16 @@ int main() {
     int opcion;
     while (1) {
         printf("\n--- Menú Principal ---\n");
-        printf("1. Jugar con vectores (strings) \n2. Jugar con matrices (multiplicar dos matrices de un tamaño especificado)\n3. Salir\nOpción: ");
+        printf("1. Jugar con vectores (strings) \n2. Jugar con matrices (multiplicar dos matrices de un tamaño especificado)\n3. Salir\nOpción: ");//Opciones menu principal
         scanf("%d", &opcion);
         switch (opcion) {
-            case 1:
+            case 1:             //En caso de seleccionar la opcion 1 llamar al menu de vectores
                 vectorMenu(&V);
                 break;
-            case 2:
+            case 2:             //En caso de seleccionar la opcion 2 llamar al menu de matrices
                 matrizMenu();
                 break;
-            case 3:
+            case 3:             //En caso de salir, asegurarse de liberar la memoria del vector inicializado previamente
                 freeVector(&V);
                 return 0;
         }
--- Taller_Compilacion_Modular/vector_dinamico.c
+++ Taller_Compilacion_Modular/vector_dinamico.c
@@ -13,16 +13,18 @@
 
 #define CAPACIDAD_INICIAL 4
 
+
+// Inicializa un vector dinámico con capacidad inicial
 void vectorInicio(vectorDinamico *V) {
     V->capacidad = CAPACIDAD_INICIAL;
     V->totalElementos = 0;
     V->elementos = malloc(sizeof(void *) * V->capacidad);
 }
-
+// Devuelve el número total de elementos
 int totalVector(vectorDinamico *V) {
     return V->totalElementos;
 }
-
+// Añade un elemento al vector, redimensionando si es necesario
 static void resizeVector(vectorDinamico *V, int capacidad) {
     printf("Redimensión: %d a %d\n", V->capacidad, capacidad);
     void **elementos = realloc(V->elementos, sizeof(void *) * capacidad);
@@ -31,28 +33,28 @@ static void resizeVector(vectorDinamico *V, int capacidad) {
         V->capacidad = capacidad;
     }
 }
-
+// Añade un elemento al vector, redimensionando si es necesario
 void addVector(vectorDinamico *V, void *elemento) {
     if (V->capacidad == V->totalElementos)
         resizeVector(V, V->capacidad * 2);
     V->elementos[V->totalElementos++] = elemento;
 }
-
+// Libera la memoria del vector
 void freeVector(vectorDinamico *V) {
     free(V->elementos);
 }
-
+// Obtiene un elemento en un índice dado
 void *getVector(vectorDinamico *V, int indice) {
     if (indice >= 0 && indice < V->totalElementos)
         return V->elementos[indice];
     return NULL;
 }
-
+// Establece un elemento en un índice dado
 void setVector(vectorDinamico *V, int indice, void *elemento) {
     if (indice >= 0 && indice < V->totalElementos)
         V->elementos[indice] = elemento;
 }
-
+// Borra un elemento en un índice dado y ajusta el vector
 void borrarVector(vectorDinamico *V, int indice) {
     if (indice < 0 || indice >= V->totalElementos)
         return;
@@ -65,7 +67,7 @@ void borrarVector(vectorDinamico *V, int indice) {
     if (V->totalElementos > 0 && V->totalElementos == V->capacidad / 4)
         resizeVector(V, V->capacidad / 2);
 }
-
+//Imprime el vector
 void imprimirVector(vectorDinamico *V){
     for (int i = 0; i < totalVector(V); i++)
                     printf("%s ", (char *)getVector(V, i));
